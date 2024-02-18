fe3404f4fc849321a54b19493a882cdd0892bf80
sarifor <iread1q84@gmail.com>
2024-02-18 17:53:59 +0900

    Fix comment

--- 02-JS-basics/promise/promise-basics/multiple-promises-at-once.js
+++ 02-JS-basics/promise/promise-basics/multiple-promises-at-once.js
@@ -1,8 +1,8 @@
+// 즉시 resolve하는 프로미스를 만드는 방법
 const promise1 = Promise.resolve('Success 1');
 const promise2 = Promise.resolve('Success 2');
 
-// 프로미스 여러 개를 한 번에 실행
-// - 즉시 resolve하는 프로미스를 만드는 방법
+// 프로미스 여러 개를 한 번에 실행 
 // - 모든 프로미스가 resolve될 때까지 기다렸다가 then으로 넘어감
 Promise.all([promise1, promise2])
   .then((result) => {
