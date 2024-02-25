5fada817a48e6360d053d00b40908ca9c9812724
sarifor <iread1q84@gmail.com>
2024-02-25 17:38:07 +0900

    Practice __filename and __dirname

---
+++ 03-Node.js-Functions/03-04-Built-in-objects/03-04-04-__filename-__dirname/__filename-__dirname-basics.js
@@ -0,0 +1,2 @@
+console.log(__filename); // C:\(중략)\03-04-04-__filename-__dirname\__filename-__dirname-basics.js  // Windows라 '\'로 폴더 경로 구분됨
+console.log(__dirname); // C:\(중략)\03-04-04-__filename-__dirname // 상동
