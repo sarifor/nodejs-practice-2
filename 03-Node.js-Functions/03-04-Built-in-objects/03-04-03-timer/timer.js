f0043847886aa829095a010cc12336ea91d3db6e
sarifor <iread1q84@gmail.com>
2024-02-25 17:03:45 +0900

    Practice setTimeout/setInterval

---
+++ 03-Node.js-Functions/03-04-Built-in-objects/03-04-03-timer/timer.js
@@ -0,0 +1,32 @@
+const timeout = setTimeout(() => { // 3rd
+  console.log('Execute in 1.5 sec');
+}, 1500);
+
+const interval = setInterval(() => { // 2nd, 4th
+  console.log("Execute every 1 sec");
+}, 1000);
+
+const timeout2 = setTimeout(() => {
+  console.log("Does not execute");
+}, 3000);
+
+setTimeout(() => {
+  clearTimeout(timeout2);
+  clearInterval(interval);
+}, 2500);
+
+const immediate = setImmediate(() => { // 1st
+  console.log("Execute right now");
+});
+
+const immediate2 = setImmediate(() => {
+  console.log("Does not execute");
+});
+
+clearImmediate(immediate2);
+
+// Result:
+// Execute right now
+// Execute every 1 sec
+// Execute in 1.5 sec
+// Execute every 1 sec
