4553190294ad3d7e43946a5f32683aeeef18ddc2
sarifor <iread1q84@gmail.com>
2024-02-29 11:17:08 +0900

    Fix mistake

--- 03-Node.js-Functions/03-04-Built-in-objects/03-04-06-process/process.env/1.js
+++ 03-Node.js-Functions/03-04-Built-in-objects/03-04-06-process/process.env/1.js
@@ -5,4 +5,4 @@ console.log(process.env.test); // hi
 
 // - Delete data
 delete process.env.test // Javascript delete operator // does work
-console.log(process.env.test); // hi
+console.log(process.env.test); // undefined
