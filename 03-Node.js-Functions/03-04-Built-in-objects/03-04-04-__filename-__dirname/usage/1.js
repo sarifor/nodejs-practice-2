f805fdab292930ef3b70ef5fa1384d8028f11980
sarifor <iread1q84@gmail.com>
2024-05-04 16:30:48 +0900

    Rename/relocate files of __filename/__dirname

--- 03-Node.js-Functions/03-04-Built-in-objects/03-04-04-__filename-__dirname/usage/1.js
+++ 03-Node.js-Functions/03-04-Built-in-objects/03-04-04-__filename-__dirname/usage/1.js
@@ -1,2 +1,6 @@
+// # __filename, __dirname
+// - 경로 정보 취득
+// - 보통은 path 모듈과 함께 사용
+
 console.log(__filename); // C:\(중략)\03-04-04-__filename-__dirname\__filename-__dirname-basics.js  // Windows라 '\'로 폴더 경로 구분됨
 console.log(__dirname); // C:\(중략)\03-04-04-__filename-__dirname // 상동
