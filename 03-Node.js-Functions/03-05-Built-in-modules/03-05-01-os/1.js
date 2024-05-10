59648eafe2735500bf459168d3d37c11c44531fc
sarifor <iread1q84@gmail.com>
2024-05-10 15:24:48 +0900

    Practice os module

--- 03-Node.js-Functions/03-05-Built-in-modules/03-05-01-os/1.js
+++ 03-Node.js-Functions/03-05-Built-in-modules/03-05-01-os/1.js
@@ -1,5 +1,9 @@
-// os module
+// # os 모듈
 // - The node:os module provides operating system-related utility methods and properties.
+// - process 객체와 겹치는 부분도 있음
+
+// require로 모듈을 불러오지 않으면, 에러 남
+// ReferenceError: os is not defined
 const os = require('os');
 
 console.log(os);
