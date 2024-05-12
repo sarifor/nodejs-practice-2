6d6860e6feade79a9e7d589a34fd77e67d051c7d
sarifor <iread1q84@gmail.com>
2024-05-12 10:47:13 +0900

    Add comments

--- 03-Node.js-Functions/03-05-Built-in-modules/03-05-08-child_process/exec/1.js
+++ 03-Node.js-Functions/03-05-Built-in-modules/03-05-08-child_process/exec/1.js
@@ -3,7 +3,9 @@
 
 const exec = require('child_process').exec;
 
-const process = exec('cmd /c chcp 65001>nul && dir'); // UTF-8 인코딩으로 dir 실행
+// 현재 디렉토리의 파일 및 폴더 목록을 UTF-8로 출력
+// 참고: https://youtu.be/MCCC6umH-w8?si=7pNxRqYAwr22Y-XP&t=145
+const process = exec('cmd /c chcp 65001>nul && dir');
 
 process.stdout.on('data', function(data) {
   console.log(data.toString());
