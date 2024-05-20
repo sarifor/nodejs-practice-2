2ff2c99f24641897354459d61d5f6ceda3331b90
sarifor <iread1q84@gmail.com>
2024-05-20 18:13:31 +0900

    Update comment

--- 03-Node.js-Functions/03-06-File-system/03-06-00-what-is-fs/basic-methods-properties/readFile/1.js
+++ 03-Node.js-Functions/03-06-File-system/03-06-00-what-is-fs/basic-methods-properties/readFile/1.js
@@ -1,6 +1,6 @@
 // # fs 모듈의 readFile 메서드
 // - 용도: 파일 읽기
-// - 결과물: Buffer 형식 => 문자열 형식 by toString 메서드
+// - 결과물: Buffer 형식
 
 const fs = require('fs');
 
@@ -9,5 +9,5 @@ fs.readFile(`${__dirname}/readme1.txt`, (err, data) => { // 파일을 읽은 후
     throw err;
   }
   console.log(data); // <Buffer 43 6f 70 65 72 6e 69 63 75 73 21 20 53 61 76 65 20 6d 65 21>
-  console.log(data.toString()); // Copernicus! Save me!
+  console.log(data.toString()); // Copernicus! Save me!  // 문자열 형식 by toString 메서드
 });
